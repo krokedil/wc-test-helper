ae15ea50d17f1775a8d14814a440fb2b8612cdbd
MichaelBengtsson <michael@krokedil.se>
2023-01-25 15:07:14 +0100

    Add more helper methods. To get system report and to login as admin

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-import { APIRequestContext, request } from "@playwright/test";
+import { APIRequestContext, Page, request } from "@playwright/test";
+
+const {
+    ADMIN_USERNAME,
+    ADMIN_PASSWORD,
+} = process.env;
+
+export const AdminLogin = async (page: Page) => {
+    await page.goto('/wp-admin');
+    await page.locator('#user_login').click();
+    await page.locator('#user_login').fill(ADMIN_USERNAME ?? 'admin');
+    await page.locator('#user_pass').click();
+    await page.locator('#user_pass').fill(ADMIN_PASSWORD ?? 'password');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.waitForLoadState('networkidle');
+}
 
 export const GetWcApiClient = async ( baseURL: string, consumerKey: string, consumerSecret: string ): Promise<APIRequestContext> => {
     return await request.newContext({
@@ -9,4 +24,24 @@ export const GetWcApiClient = async ( baseURL: string, consumerKey: string, cons
             ).toString('base64')}`,
         },
     });
+}
+
+export const GetSystemReportData = async (wcApiClient: APIRequestContext) => {
+    // Get the system status.
+    const response = await wcApiClient.get('system_status');
+    const status = await response.json();
+
+    // Set the environment variables.
+    process.env.WC_VERSION = status.environment.version.trim().split(' ')[0];
+    process.env.WP_VERSION = status.environment.wp_version.trim().split(' ')[0];
+    process.env.PHP_VERSION = status.environment.php_version.trim().split(' ')[0];
+
+    // Get the name and plugin version of the plugin from the active_plugins.
+    const activePlugins = status.active_plugins;
+    const plugin = activePlugins.find((activePlugin: any) => activePlugin.plugin.includes('klarna-payments-for-woocommerce'));
+
+    if (plugin) {
+        process.env.PLUGIN_NAME = plugin.name;
+        process.env.PLUGIN_VERSION = plugin.version;
+    }
 }
